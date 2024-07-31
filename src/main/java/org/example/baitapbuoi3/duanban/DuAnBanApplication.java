bd36813d15be97f9b9571e311bffa50522190d1c
manhdinh2054 <manhdinh523@gmail.com>
2024-07-31 18:40:14 +0700

    addall

---
+++ src/main/java/org/example/baitapbuoi3/duanban/DuAnBanApplication.java
@@ -0,0 +1,13 @@
+package org.example.baitapbuoi3.duanban;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DuAnBanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DuAnBanApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/baitapbuoi3/duanban/DuAnBanApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.baitapbuoi3.duanban;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DuAnBanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
