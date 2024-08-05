db2d0172618d4b35ef01a5f8a27c7ea0fd4e19fd
manhdinh2054 <manhdinh523@gmail.com>
2024-08-05 14:57:28 +0700

    addall

--- src/main/java/org/example/baitapbuoi3/duanban/Controller/SanPhamDetail.java
+++ src/main/java/org/example/baitapbuoi3/duanban/Controller/SanPhamDetail.java
@@ -1,18 +1,41 @@
 package org.example.baitapbuoi3.duanban.Controller;
 
+import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
+import org.example.baitapbuoi3.duanban.Entity.Account;
+import org.example.baitapbuoi3.duanban.Entity.Category;
+import org.example.baitapbuoi3.duanban.Entity.Product;
+import org.example.baitapbuoi3.duanban.Repository.CategoryInterface;
+import org.example.baitapbuoi3.duanban.Repository.SanPhamDetailInterface;
+import org.example.baitapbuoi3.duanban.Repository.SanPhamInterface;
+import org.example.baitapbuoi3.duanban.Services.SanPham;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
 @RequestMapping("/trangchu.com")
 public class SanPhamDetail {
+    final SanPhamDetailInterface spdi;
+    final CategoryInterface cgi;
 
-    @RequestMapping("detail")
-    public String detail(Model model) {
+    @RequestMapping("detail/{id}")
+    public String detail(Model model, @PathVariable int id, HttpSession session) {
+        List<Product> lst = spdi.FindById1(id);
+        List<Category> lstct = cgi.findAll();
+        Account account = (Account) session.getAttribute("loginuser");
+        if(account != null) {
+            model.addAttribute("account1", account);
 
+        }else {
+            return "redirect:/login";
+        }
+        model.addAttribute("productdetaillst", lst);
+        model.addAttribute("category",lstct);
         return "sanphamdetail";
     }
 }
---
+++ src/main/java/org/example/baitapbuoi3/duanban/Repository/SanPhamDetailInterface.java
@@ -0,0 +1,14 @@
+package org.example.baitapbuoi3.duanban.Repository;
+
+import org.example.baitapbuoi3.duanban.Entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface SanPhamDetailInterface extends JpaRepository<Product,Integer> {
+    @Query(value = "select * from Products where id = :id",nativeQuery = true)
+    List<Product> FindById1(int id);
+}
